809c4dc662dd211b5396c0f9c96f605f894266bd
Víctor <victorsisoncheca@gmail.com>
2023-02-19 20:13:15 +0100

    Exportar lista de clientes a su componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,80 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { CustomersService } from './services/customers.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-
-  customers: any;
-
-  cus = {
-    id: 0,
-    nombre: "",
-    apellidos: ""
-    /*  nombre_fiscal: '',
-     nif_cif: '',
-     direccion: '',
-     direccion_fiscal: '',
-     codigo_postal: 0,
-     poblacion: '',
-     provincia: '',
-     email: '',
-     pagina_web: '',
-     cuenta_corriente: '',
-     telefono: '',
-     telefono_2: '',
-     telefono_3: '',
-     contacto: '',
-     contacto_2: '' */
-  }
-
-  constructor(private customersService: CustomersService) { }
-
-  ngOnInit() {
-    this.recuperarTodos();
-  }
-
-  recuperarTodos() {
-    this.customersService.recuperarTodos().subscribe((result: any) => this.customers = result);
-  }
-
-  public alta() {
-    this.customersService.alta(this.cus).subscribe((datos: any) => {
-      if (datos['resultado'] == 'OK') {
-        alert(datos['mensaje']);
-        this.recuperarTodos();
-      }
-    });
-  }
-
-  public baja(id: number) {
-    this.customersService.baja(id).subscribe((datos: any) => {
-      if (datos['resultado'] == 'OK') {
-        alert(datos['mensaje']);
-        this.recuperarTodos();
-      }
-    });
-  }
-
-  public modificacion() {
-    this.customersService.modificacion(this.cus).subscribe((datos: any) => {
-      if (datos['resultado'] == 'OK') {
-        alert(datos['mensaje']);
-        this.recuperarTodos();
-      }
-    });
-  }
-
-  public seleccionar(id: number) {
-    this.customersService.seleccionar(id).subscribe((result: any) => this.cus = result[0]);
-  }
-
-  public hayRegistros() {
-    return true;
-  }
 
 }
--- src/app/customer/customer-list/customer-list.component.ts
+++ src/app/customer/customer-list/customer-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomersService } from 'src/app/services/customers.service';
 
 @Component({
   selector: 'customer-list',
@@ -7,9 +8,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerListComponent implements OnInit {
 
-  constructor() { }
+  customers: any;
 
-  ngOnInit(): void {
+  cus = {
+    id: 0,
+    nombre: "",
+    apellidos: ""
+    /*  nombre_fiscal: '',
+     nif_cif: '',
+     direccion: '',
+     direccion_fiscal: '',
+     codigo_postal: 0,
+     poblacion: '',
+     provincia: '',
+     email: '',
+     pagina_web: '',
+     cuenta_corriente: '',
+     telefono: '',
+     telefono_2: '',
+     telefono_3: '',
+     contacto: '',
+     contacto_2: '' */
+  }
+
+  constructor(private customersService: CustomersService) { }
+
+  ngOnInit() {
+    this.recuperarTodos();
+  }
+
+  recuperarTodos() {
+    this.customersService.recuperarTodos().subscribe((result: any) => this.customers = result);
+  }
+
+  public alta() {
+    this.customersService.alta(this.cus).subscribe((datos: any) => {
+      if (datos['resultado'] == 'OK') {
+        alert(datos['mensaje']);
+        this.recuperarTodos();
+      }
+    });
+  }
+
+  public baja(id: number) {
+    this.customersService.baja(id).subscribe((datos: any) => {
+      if (datos['resultado'] == 'OK') {
+        alert(datos['mensaje']);
+        this.recuperarTodos();
+      }
+    });
+  }
+
+  public modificacion() {
+    this.customersService.modificacion(this.cus).subscribe((datos: any) => {
+      if (datos['resultado'] == 'OK') {
+        alert(datos['mensaje']);
+        this.recuperarTodos();
+      }
+    });
+  }
+
+  public seleccionar(id: number) {
+    this.customersService.seleccionar(id).subscribe((result: any) => this.cus = result[0]);
+  }
+
+  public hayRegistros() {
+    return true;
   }
 
 }
